7277f597ae68b4d31d86889a5e906328e4dfb768
Boris <lbomarleyl@gmail.com>
2021-10-04 19:39:39 +0300

    add packages.config, correct namespaceservice using attempt

---
+++ addressbook-web-tests/AppManager/APIHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace mantis_tests
+{
+    public class APIHelper : HelperBase
+    {
+        public APIHelper(ApplicationManager manager) : base(manager) { }
+        
+        public List<ProjectData> GetAllProjects()
+        {
+            List<ProjectData> allProjects = new List<ProjectData>();
+            AccountData account = new AccountData("administrator", "password");
+
+            Mantis.MantisConnectPortTypeClient client = new Mantis.MantisConnectPortTypeClient();
+            List<ProjectData> projectList = new List<ProjectData>();
+
+            client.mc_projects_get_user_accessible(account.Username, account.Password);
+            return allProjects;
+        }
+    }
+}
